f1f9a29137cb4f1333260988dccb56ae80395d62
RakanNimer <rakannimer@gmail.com>
2016-12-26 10:09:08 +0200

    Moved files around for native to work with npm

--- native/index.js
+++ native/index.js
@@ -1,1 +1,15 @@
-require('./src/');
+import Note from './src/components/Note';
+import Instrument from './src/components/Instrument';
+import Orchestra from './src/components/Orchestra';
+
+export default {
+  Instrument,
+  Note,
+  Orchestra,
+};
+
+export {
+  Instrument,
+  Note,
+  Orchestra,
+};
