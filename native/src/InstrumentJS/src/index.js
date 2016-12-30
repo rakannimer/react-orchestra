3cd0ed244a1da507eedfb5c78bac8f9b98645b37
RakanNimer <rakannimer@gmail.com>
2016-12-30 12:02:30 +0200

    Remove unneeded deps

--- native/src/InstrumentJS/src/index.js
+++ native/src/InstrumentJS/src/index.js
@@ -1,15 +1,7 @@
-import AudioContext from './AudioContext';
-import Instrument from './Instruments';
 // import Melody from './Melody';
 // import Musician from './Musician';
-import Note from './Note';
-import INSTRUMENT_NAMES from './constants/INSTRUMENTS';
 import InstrumentHelpers from './helpers/';
 
 export {
-  AudioContext,
-  Instrument,
-  Note,
-  INSTRUMENT_NAMES,
   InstrumentHelpers,
 }
