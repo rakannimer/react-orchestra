4357aeb4a4dd6e87929a6ffa1b91e1d901b8619f
RakanNimer <rakannimer@gmail.com>
2017-01-09 14:44:16 +0200

    refactored note scu

--- native/src/components/Note.js
+++ native/src/components/Note.js
@@ -39,12 +39,15 @@ class Note extends React.PureComponent {
       await this.stopPlayingNote();
     }
   }
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   // TODO: split into consts
-  //   // return true;
-  //   const shouldUpdate = this.state.isLoading || (nextProps.name !== this.props.name) || (nextProps.instrumentName !== this.props.instrumentName) || (nextState.isPlaying !== this.state.isPlaying) || (nextProps.play !== this.props.play) || this.state.isPlaying || this.state.isPlaying !== nextState.isPlaying;
-  //   return shouldUpdate;
-  // }
+  shouldComponentUpdate(nextProps, nextState) {
+    // TODO: split into consts
+    // return true;
+    const isLoading = this.state.isLoading || nextState.isLoading;
+    const isPlaying = this.state.isPlaying !== nextState.isPlaying;
+    const isPlayingControlled = this.props.play !== nextProps.play;
+    const shouldUpdate = isLoading || isPlaying || isPlayingControlled; // this.state.isLoading || (nextProps.name !== this.props.name) || (nextProps.instrumentName !== this.props.instrumentName) || (nextState.isPlaying !== this.state.isPlaying) || (nextProps.play !== this.props.play) || this.state.isPlaying || this.state.isPlaying !== nextState.isPlaying;
+    return shouldUpdate;
+  }
   componentWillUnmount() {
     this.sound.release();
   }
